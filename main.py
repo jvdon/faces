b2ad3544c695b0e3632c412330137556e94ee947
João Victor <joaovictordonato2@gmail.com>
2024-04-23 10:11:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 import random
+import numpy as np
 
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-video_capture = cv2.VideoCapture("teste2.mp4")
+video_capture = cv2.VideoCapture("teste_face_only.mp4")
+
+filter = "none"
 
 while True:
     ret, frame = video_capture.read()
@@ -18,26 +21,54 @@ while True:
 
     for (x, y, w, h) in faces:
         face_region = frame[y:y+h, x:x+w]
-        # if (random.random() > 0.5):
-        #     colored_face = cv2.cvtColor(face_region, cv2.COLOR_BGR2HSV)
-        #     # Modify the HSV values as per your desired color
-        #     colored_face[:, :, 0] += random.randint(0, 180)
-        #     colored_face = cv2.cvtColor(colored_face, cv2.COLOR_HSV2RGB)
-        #     frame[y:y+h, x:x+w] = colored_face
-        # else:
-        frame[y:y+h, x:x +
+       
+        if filter == "blur":
+            frame[y:y+h, x:x +
                   w] = cv2.GaussianBlur(frame[y:y+h, x:x+w], (63, 63), 0)
+        elif filter == "color":
+            colored_face = cv2.cvtColor(face_region, cv2.COLOR_BGR2HSV)
+            # Modify the HSV values as per your desired color
+            colored_face[:, :, 0] += random.randint(0, 180)
+            frame[y:y+h, x:x+w] = colored_face
+        elif filter == "blowout":
+            frame[y:y+h, x:x+w] = cv2.dilate(face_region, np.ones((17, 17), dtype=np.uint8))
+        elif filter == "blowin":
+            frame[y:y+h, x:x+w] = cv2.erode(face_region, np.ones((17, 17), dtype=np.uint8))
 
-        # background_image[y:y+h, x:x+w] = face_region
-        # faceC = random.randint(0, 10)
-        # cv2.imshow(f"Face ~> {faceC}", face_region)
+    if(filter == "canny"):
+        edges = cv2.Canny(frame, threshold1=50, threshold2=150)
+        rgb = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB) # RGB for matplotlib, BGR for imshow() !
+         # step 2: now all edges are white (255,255,255). to make it red, multiply with another array:
+        rgb *= np.array((0,1, 0),np.uint8) # set g and b to 0, leaves red :)
+         # step 3: compose:
+        # out = np.bitwise_or(frame, rgb)
+        frame = rgb
+    
 
-        # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
+    
+    cv2.putText(frame, f"Filter: {filter}", (200, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+    
     cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    
+    
+    if(key == ord("q")):
+        print(cv2.waitKey(1) & 0xFF)
         break
+    elif key == ord("a"):
+        filter = "blur"
+    elif key == ord("s"):
+        filter = "color"
+    elif key == ord("d"):
+        filter = "canny"
+    elif key == ord("f"):
+        filter = "blowout"
+    elif key == ord("g"):
+        filter = "blowin"
+    elif key == ord("l"):
+        filter =  "none"
+    
 
 video_capture.release()
 cv2.destroyAllWindows()
